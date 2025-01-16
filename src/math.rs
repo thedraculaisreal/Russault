12ca78c02e80622c414b02c50d6dade6159d0cd3
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-16 10:33:28 -0800

    adding math calculations

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,5 @@
+use libm;
+
 #[derive(Default)]
 pub struct Vec3 {
     pub x: f32,
@@ -20,4 +22,30 @@ impl Vec3 {
             z,
         }
     }
+    fn calc_length(&mut self) -> f32{
+        libm::sqrtf(self.x * self.x + self.y * self.y + self.z * self.z)
+    }
+    pub fn calc_distance(&mut self, target: Vec3) -> f32 {
+        self.x -= target.x;
+        self.y -= target.y;
+        self.z -= target.z;
+        self.calc_length()
+    }
+}  
+
+const PI: f32 = 3.14159;
+
+fn radians_to_degrees(radians_angle: f32) -> f32 {
+    radians_angle * (180.0 / PI)
+}
+
+pub fn calculate_angle(mut origin: Vec3, mut target: Vec3) -> Vec3 {
+    let mut results: Vec3 = Vec3::new(0.0,0.0,0.0);
+    results.y = radians_to_degrees(-libm::atan2f(target.x - origin.y, target.y - origin.y));
+    if results.y <= 90.0 {
+        results.y += 360.0;
+    }
+    results.y -= 270.0;
+    results.x = -radians_to_degrees(libm::asinf((target.z - origin.z) / origin.calc_distance(target)));
+    return results
 }
