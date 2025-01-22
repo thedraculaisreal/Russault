dbeaedab0d5434d4e63afb85c6a035bbd022f359
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-22 13:47:47 -0800

    makign window transparent

--- src/overlay.rs
+++ src/overlay.rs
@@ -1,9 +1,19 @@
 extern crate glium;
 use glium::winit;
+use crate::overlay::glium::Surface;
+// my source for learning glium and glutin https://github.com/glium/glium/blob/master/book/tuto-01-getting-started.md
 
 pub fn create_overlay() {
     let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+    // making window always on top and transparent.
+    window.set_window_level(winit::window::WindowLevel::AlwaysOnTop);
+    window.set_transparent(true);
+    // drawing
+    let mut frame = display.draw();
+    // all 0s including alpha to make window transparent.
+    frame.clear_color(0.0, 0.0, 0.0, 0.0);
+    frame.finish().unwrap();
     // event_loop
     let _ = event_loop.run(move |event, window_target| {
 	match event {
