7618a7a6f4bed84885c27dd2499115d3750af09e
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-22 13:36:00 -0800

    adding opengl overlay

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,10 @@ mod cheats;
 mod overlay;
 
 fn main() -> io::Result<()> {
-    let game = Process::with_name("ac_client.exe").expect("Failed to find game");
     thread::spawn(move || {
-	overlay::create_overlay();
-	// message handling and render pipeline will go here.
+	let game = Process::with_name("ac_client.exe").expect("Failed to find game");
+	entities::entity_list_loop(&game);
     });
-    entities::entity_list_loop(&game);
+    overlay::create_overlay();
     Ok(())
 }
--- src/overlay.rs
+++ src/overlay.rs
@@ -1,40 +1,17 @@
-use windows::{ core::*, Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*,};
-use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+extern crate glium;
+use glium::winit;
 
-pub fn create_overlay() -> HWND {
-    unsafe {
-	// we use none in getmodulehandle to get the handle to our window that we are creating.
-	let h_instance = GetModuleHandleW(None).unwrap().into();
-	let class_name = "overlay_class";
-	let overlay_name = "russault_overlay";
-
-	let wc = WNDCLASSW {
-	    hInstance: h_instance,
-	    // PWSTR is a windows pointer to wide string, which is a pointer mutable utf16 chars.
-	    lpszClassName: PCWSTR(class_name.encode_utf16().chain(Some(0)).collect::<Vec<u16>>().as_mut_ptr()),
-	    lpfnWndProc: Some(WNDPROC),
-	    style: CS_HREDRAW | CS_VREDRAW,
-	    // defaulting rest of params that we dont care about.
-	    ..Default::default()
+pub fn create_overlay() {
+    let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+    // event_loop
+    let _ = event_loop.run(move |event, window_target| {
+	match event {
+	    glium::winit::event::Event::WindowEvent { event, .. } => match event {
+		glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+		_ => (),
+	    },
+	    _ => (),
 	};
-	RegisterClassW(&wc);
-
-	let hwnd = CreateWindowExW(
-	    // we want our overlay to sit on top of everything and be transparent.
-	    WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST,
-	    PWSTR(class_name.encode_utf16().chain(Some(0)).collect::<Vec<u16>>().as_mut_ptr()),
-	    PWSTR(overlay_name.encode_utf16().chain(Some(0)).collect::<Vec<u16>>().as_mut_ptr()),
-	    WS_POPUP,
-	    0,0,800,600,
-	    Some(HWND(std::ptr::null_mut())),
-	    Some(HMENU(std::ptr::null_mut())),
-	    Some(h_instance),
-	    Some(std::ptr::null_mut()),
-	);
-	// LWA_ALPHA sets are opacity of backgroudn to be transparent.
-	SetLayeredWindowAttributes(hwnd.expect("REASON"), windows::Win32::Foundation::COLORREF(0), 255, LWA_ALPHA);
-	ShowWindow(hwnd.expect("REASON"), SW_SHOW);
-	// returning our hwnd so we can use it in our drawing with wgpu later, once ive gotten to it.
-	hwnd.expect("REASON")
-    }
+    });
 }
